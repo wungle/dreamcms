a427ac781be9016fce7e858f96264f1ae3e6dac7
Richan Fongdasen <richan.fongdasen@yahoo.com>
2013-12-20 18:22:51 +0700

    Baked: Icon model

---
+++ app/Plugin/DreamCms/Test/Fixture/IconFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * IconFixture
+ *
+ */
+class IconFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 128, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => '',
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created' => '2013-12-20 17:52:23',
+			'modified' => '2013-12-20 17:52:23'
+		),
+	);
+
+}
