1ab7b086789f08d3701e7a52934b50f4722599b1
Richan Fongdasen <richan.fongdasen@yahoo.com>
2014-01-03 18:06:39 +0700

    Updated: PhotoGalleriesAppController

--- app/Plugin/PhotoGalleries/Controller/PhotoGalleriesAppController.php
+++ app/Plugin/PhotoGalleries/Controller/PhotoGalleriesAppController.php
@@ -3,5 +3,49 @@
 App::uses('AppController', 'Controller');
 
 class PhotoGalleriesAppController extends AppController {
+	public $theme = 'AceAdmin';
 
+	public $components = array(
+		'Session',
+		'Dreamcms.DreamcmsAcl',
+		'Dreamcms.Translator',
+		'Dreamcms.Routeable',
+		'Dreamcms.DreamcmsAuth' => array(
+			'userModel' => 'Dreamcms.Admin',
+			'authenticate' => array(
+				'Dreamcms.Dreamcms' => array(
+					'userModel' => 'Dreamcms.Admin',
+					'sessionKey' => 'DreamCMS.Admin',
+					'passwordHasher' => 'Blowfish'
+				)
+			)
+		),
+		'DataFinder',
+	);
+
+	public $helpers = array(
+		'Html',
+		'Form',
+		'Number',
+		'Paginator',
+		'Session',
+		'Text',
+		'Time',
+		'Dreamcms.CoreDreamCms',
+		'Dreamcms.DreamcmsForm',
+		'Dreamcms.Routeable',
+	);
+
+	public function beforeFilter()
+	{
+		parent::beforeFilter();
+
+		if ((strpos($this->params->url, 'dreamcms') === 0)  && !$this->DreamcmsAuth->user())
+			throw new NotFoundException();
+	}
+
+	public function beforeRender()
+	{
+		parent::beforeRender();
+	}
 }
