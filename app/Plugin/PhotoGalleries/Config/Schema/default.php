cdf7ce6ad442686db745edee9d9137fa5f4de54b
Richan Fongdasen <richan.fongdasen@yahoo.com>
2014-01-04 16:57:35 +0700

    Updated: Schema for PhotoGalleries.Default -- Include PhotoThumbnail model

--- app/Plugin/PhotoGalleries/Config/Schema/default.php
+++ app/Plugin/PhotoGalleries/Config/Schema/default.php
@@ -77,6 +77,35 @@ class DefaultSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
 	);
 
+	public $photo_thumbnails = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'thumbnail_type_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'key' => 'index', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'model' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'foreign_key' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
+		'field' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'locale' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 12, 'key' => 'index', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'filename' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'width' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
+		'height' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'thumbnail_type_id' => array('column' => 'thumbnail_type_id', 'unique' => 0),
+			'name' => array('column' => 'name', 'unique' => 0),
+			'model' => array('column' => 'model', 'unique' => 0),
+			'foreign_key' => array('column' => 'foreign_key', 'unique' => 0),
+			'field' => array('column' => 'field', 'unique' => 0),
+			'locale' => array('column' => 'locale', 'unique' => 0),
+			'common_query1' => array('column' => array('model', 'foreign_key', 'field', 'locale'), 'unique' => 0),
+			'common_query2' => array('column' => array('thumbnail_type_id', 'model', 'foreign_key', 'field', 'locale'), 'unique' => 0),
+			'common_query3' => array('column' => array('model', 'foreign_key', 'field'), 'unique' => 0),
+			'common_query4' => array('column' => array('thumbnail_type_id', 'model', 'foreign_key', 'field'), 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
 	public $photos = array(
 		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'primary'),
 		'photo_album_id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'key' => 'index'),
--- app/Plugin/PhotoGalleries/Config/Schema/default.sql
+++ app/Plugin/PhotoGalleries/Config/Schema/default.sql
@@ -3,7 +3,9 @@
 DROP TABLE IF EXISTS `photo_album_i18n`;
 DROP TABLE IF EXISTS `photo_albums`;
 DROP TABLE IF EXISTS `photo_i18n`;
+DROP TABLE IF EXISTS `photo_thumbnails`;
 DROP TABLE IF EXISTS `photos`;
+
 
 CREATE TABLE `photo_album_i18n` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
@@ -50,6 +52,32 @@ CREATE TABLE `photo_i18n` (
 	KEY `model` (`model`),
 	KEY `row_id` (`foreign_key`),
 	KEY `field` (`field`)) 	DEFAULT CHARSET=utf8,
+	COLLATE=utf8_unicode_ci,
+	ENGINE=InnoDB;
+
+CREATE TABLE `photo_thumbnails` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`thumbnail_type_id` bigint(20) NOT NULL,
+	`name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`model` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`foreign_key` bigint(20) NOT NULL,
+	`field` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`locale` varchar(12) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`filename` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`width` int(10) NOT NULL,
+	`height` int(10) NOT NULL,
+	`created` datetime DEFAULT NULL,
+	`modified` datetime DEFAULT NULL,	PRIMARY KEY  (`id`),
+	KEY `thumbnail_type_id` (`thumbnail_type_id`),
+	KEY `name` (`name`),
+	KEY `model` (`model`),
+	KEY `foreign_key` (`foreign_key`),
+	KEY `field` (`field`),
+	KEY `locale` (`locale`),
+	KEY `common_query1` (`model`, `foreign_key`, `field`, `locale`),
+	KEY `common_query2` (`thumbnail_type_id`, `model`, `foreign_key`, `field`, `locale`),
+	KEY `common_query3` (`model`, `foreign_key`, `field`),
+	KEY `common_query4` (`thumbnail_type_id`, `model`, `foreign_key`, `field`)) 	DEFAULT CHARSET=utf8,
 	COLLATE=utf8_unicode_ci,
 	ENGINE=InnoDB;
 
