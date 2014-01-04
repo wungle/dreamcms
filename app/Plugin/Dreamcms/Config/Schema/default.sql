17c708b5575b7c90f816cb4234855b0036f98c11
Richan Fongdasen <richan.fongdasen@yahoo.com>
2014-01-04 16:44:15 +0700

    Updated: Schema for Dreamcms.Default -- Include Thumbnail model

--- app/Plugin/Dreamcms/Config/Schema/default.sql
+++ app/Plugin/Dreamcms/Config/Schema/default.sql
@@ -14,6 +14,7 @@ DROP TABLE IF EXISTS `languages`;
 DROP TABLE IF EXISTS `settings`;
 DROP TABLE IF EXISTS `temp_dirs`;
 DROP TABLE IF EXISTS `thumbnail_types`;
+DROP TABLE IF EXISTS `thumbnails`;
 DROP TABLE IF EXISTS `web_menu_i18n`;
 DROP TABLE IF EXISTS `web_menus`;
 
@@ -225,6 +226,32 @@ CREATE TABLE `thumbnail_types` (
 	KEY `name` (`name`),
 	KEY `method` (`method`),
 	KEY `deleted` (`deleted`)) 	DEFAULT CHARSET=utf8,
+	COLLATE=utf8_unicode_ci,
+	ENGINE=InnoDB;
+
+CREATE TABLE `thumbnails` (
+	`id` bigint(20) NOT NULL AUTO_INCREMENT,
+	`thumbnail_type_id` bigint(20) NOT NULL,
+	`name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`model` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`foreign_key` bigint(20) NOT NULL,
+	`field` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`locale` varchar(12) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`filename` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`width` int(10) NOT NULL,
+	`height` int(10) NOT NULL,
+	`created` datetime DEFAULT NULL,
+	`modified` datetime DEFAULT NULL,	PRIMARY KEY  (`id`),
+	KEY `thumbnail_type_id` (`thumbnail_type_id`),
+	KEY `name` (`name`),
+	KEY `model` (`model`),
+	KEY `foreign_key` (`foreign_key`),
+	KEY `field` (`field`),
+	KEY `locale` (`locale`),
+	KEY `common_query1` (`model`, `foreign_key`, `field`, `locale`),
+	KEY `common_query2` (`thumbnail_type_id`, `model`, `foreign_key`, `field`, `locale`),
+	KEY `common_query3` (`model`, `foreign_key`, `field`),
+	KEY `common_query4` (`thumbnail_type_id`, `model`, `foreign_key`, `field`)) 	DEFAULT CHARSET=utf8,
 	COLLATE=utf8_unicode_ci,
 	ENGINE=InnoDB;
 
