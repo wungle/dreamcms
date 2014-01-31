3198e07b0cade3b8138e6eaca970fcd78cd94166
Richan Fongdasen <richan.fongdasen@yahoo.com>
2014-01-31 14:11:31 +0700

    Updated: Core Config - CakePHP version

--- app/Plugin/Dreamcms/Config/core.php
+++ app/Plugin/Dreamcms/Config/core.php
@@ -2,6 +2,6 @@
 
 Configure::write('DreamCMS.cipher', '$2a$10$HouJypW5muo35IODDBoZoOGX5pClfnKmK5b59LyKd.suMYNDcmTbC$2a$10$DI70p3OpKwqafxOmPIFJT.9NFlVjBCRQhRzXf0kuNzZ/AZEN0lVSS');
 Configure::write('DreamCMS.version', '0.0.0');
-Configure::write('DreamCMS.Cake.version', '2.4.4');
+Configure::write('DreamCMS.Cake.version', '2.4.5');
 
 ?>
