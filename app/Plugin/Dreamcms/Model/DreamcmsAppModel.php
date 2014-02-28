bbdde268fd17518ee3db4b97e3bf50e4f6d4933e
Richan Fongdasen <richan.fongdasen@yahoo.com>
2014-02-28 11:24:22 +0700

    Fixed: no more duplicate methods in the AppModels

--- app/Plugin/Dreamcms/Model/DreamcmsAppModel.php
+++ app/Plugin/Dreamcms/Model/DreamcmsAppModel.php
@@ -4,33 +4,8 @@ App::uses('AppModel', 'Model');
 
 class DreamcmsAppModel extends AppModel {
 
-	protected $modelSchema;
-
 	protected $multiLanguageTransactionStarted = false;
 	protected $multiLanguageTransactionEnded = false;
-
-	public function beforeFind($query)
-	{
-		if (!$this->modelSchema)
-			$this->modelSchema = $this->schema();
-
-		$parentResult = parent::beforeFind($query);
-		if (is_array($parentResult))
-			$query = $parentResult;
-		elseif (!$parentResult)
-			return false;
-
-		if (isset($this->modelSchema['deleted']) && !isset($query['conditions'][$this->alias . '.deleted']) && !isset($query['conditions']['deleted']))
-		{
-			if (!isset($query['conditions']))
-				$query['conditions'] = array();
-			$query['conditions'][$this->alias . '.deleted'] = '0';
-
-			return $query;
-		}
-
-		return true;
-	}
 
 	public function resetMultiLanguageTransaction()
 	{
